f7241caad8ba4a61d4b6fbb94b4308e0184ee73c
alisha fazal <alisha_fazal@hotmail.com>
2023-10-03 15:43:31 +0000

    Add questions to script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,92 @@
-console.log("Success!");
+const questions = [
+{
+    question: "What are the two reactants of respiration?",
+    Answers: [
+        {option: "Glucose and oxygen", correct: true},
+        {option: "Glucose and water", correct: false},
+        {option: "Oxygen and water", correct: false},
+        {option: "Oxygen and carbon dioxide", correct: false},
+    ]
+},
+{
+    question: "What is the chemical element for the symbol W?",
+    Answers: [
+        {option: "Osmium", correct: false},
+        {option: "Radon", correct: false},
+        {option: "Tungsten", correct: true},
+        {option: "Nitrogen", correct: false},
+    ]
+},
+{
+    question: "Which gas makes up the third largest component of the earths atmosphere?",
+    Answers: [
+        {option: "Carbon dioxide", correct: false},
+        {option: "Argon", correct: true},
+        {option: "Oxygen", correct: false},
+        {option: "Helium", correct: false},
+    ]
+},
+{
+    question: "How many blood groups are there?",
+    Answers: [
+        {option: "4", correct: false},
+        {option: "6", correct: false},
+        {option: "8", correct: true},
+        {option: "10", correct: false},
+    ]
+},
+{
+    question: "Which is the longest nerve in the human body?",
+    Answers: [
+        {option: "Optic nerve", correct: false},
+        {option: "Ulnar nerve", correct: false},
+        {option: "Vagus nerve", correct: false},
+        {option: "Sciatic nerve", correct: true},
+    ]
+},
+{
+    question: "The asteroid belt is located between which two planets?",
+    Answers: [
+        {option: "Jupiter and Mars", correct: true},
+        {option: "Jupiter and Saturn", correct: false},
+        {option: "Mercury and Venus", correct: false},
+        {option: "Saturn and Uranus", correct: false},
+    ]
+},
+{
+    question: "Diamonds are formed of which substance?",
+    Answers: [
+        {option: "Carbon", correct: true},
+        {option: "Oxygen", correct: false},
+        {option: "Sodium", correct: false},
+        {option: "Nitrogen", correct: false},
+    ]
+},
+{
+    question: "Which metal is the lightest?",
+    Answers: [
+        {option: "Silver", correct: false},
+        {option: "Aluminium", correct: false},
+        {option: "Iron", correct: false},
+        {option: "Lithium", correct: true},
+    ]
+},
+{
+    question: "Largest mountain the the world?",
+    Answers: [
+        {option: "K2", correct: false},
+        {option: "Kilamanjaro", correct: false},
+        {option: "Mount Everest", correct: true},
+        {option: "Ben Nevis", correct: false},
+    ]
+},
+{
+    question: "Rayleigh scattering is responsible for which phenomena?",
+    Answers: [
+        {option: "Volcanos", correct: false},
+        {option: "Sky blue", correct: true},
+        {option: "Gas", correct: false},
+        {option: "Water", correct: false},
+    ]
+}
+]
