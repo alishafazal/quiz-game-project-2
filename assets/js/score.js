02a241fa5e0709a9ef987c81c60223ab9aefa6e7
alisha fazal <alisha_fazal@hotmail.com>
2023-10-09 23:09:50 +0000

    Add further text to the final score number

--- assets/js/score.js
+++ assets/js/score.js
@@ -1,3 +1,3 @@
 let finalScore = localStorage.getItem("finalScore");
 let scoreText = document.getElementById("score-text");
-scoreText.innerHTML = finalScore;
+scoreText.innerHTML = ("You scored: " + finalScore + " out of 10");
