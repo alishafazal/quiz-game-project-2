c3e8aa372d6f3aa826924d4d28f64c7515803008
alisha fazal <alisha_fazal@hotmail.com>
2023-10-12 12:31:29 +0000

    Add comment to explain the variables in score.js file

--- assets/js/score.js
+++ assets/js/score.js
@@ -1,3 +1,7 @@
+/**
+ * Get the final score from local storage and store it in the finalScore variable
+ * get the HTML element with the id of score-text and set its inner HTML as finalScore
+ */
 let finalScore = localStorage.getItem("finalScore");
 let scoreText = document.getElementById("score-text");
 scoreText.innerHTML = ("You scored: " + finalScore + " out of 10");
